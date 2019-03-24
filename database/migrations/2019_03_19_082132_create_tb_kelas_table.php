933884bc2b4d9f21f88c5eb6bc168523627cdd36
radensaleh <m.salehidris13@gmail.com>
2019-03-24 11:48:58 +0700

    fix migration kelas

--- database/migrations/2019_03_19_082132_create_tb_kelas_table.php
+++ database/migrations/2019_03_19_082132_create_tb_kelas_table.php
@@ -15,7 +15,9 @@ class CreateTbKelasTable extends Migration
     {
         Schema::create('tb_kelas', function (Blueprint $table) {
             $table->string('id_kelas', 5)->primary();
-            $table->string('kelas', 10);
+            $table->string('tingkat', 3);
+            $table->string('jurusan', 5);
+            $table->integer('rombel', false, true)->length(1);
             $table->timestamps();
         });
     }
